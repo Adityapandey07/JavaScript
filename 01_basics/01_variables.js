da08d8d824eb0834b27f62e8878146e64fc3a9e7
Adityapandey07 <87259898+Adityapandey07@users.noreply.github.com>
2024-03-10 19:06:49 +0000

    vars added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 2444
+let accountName = "Aditya"
+var accountPassword = "123$"
+accountCity = "Mumbai"
+let accountState;
+
+//we dont use var cz of its scope are wide rather than to be limited in block {}
+
+console.table([accountId,accountName, accountPassword,accountState, accountCity])
